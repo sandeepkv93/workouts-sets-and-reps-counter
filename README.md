449762a49c94561cb086455668902a793f150f97
Sandeep Vishnu <sandeepkv93@gmail.com>
2023-09-08 07:45:39 -0700

    Introduce set gap parameter

--- README.md
+++ README.md
@@ -5,15 +5,16 @@ This is a simple python script that generates an audio file that counts sets and
 ### Usage
 
 ```
-usage: sets_and_reps_counter.py [-h] --rep_gap REP_GAP --reps REPS --sets SETS [--output_file OUTPUT_FILE]
+usage: sets_and_reps_counter.py [-h] --reps REPS --sets SETS --rep_gap REP_GAP --set_gap SET_GAP [--output_file OUTPUT_FILE]
 
 Generate a guided sets and reps audio file.
 
 options:
   -h, --help            show this help message and exit
-  --rep_gap REP_GAP     The gap between reps in milliseconds.
   --reps REPS           The number of reps in each set.
   --sets SETS           The number of sets.
+  --rep_gap REP_GAP     The gap between reps in seconds.
+  --set_gap SET_GAP     The gap between sets in seconds.
   --output_file OUTPUT_FILE
                         The output mp3 file name.
 ```
@@ -23,7 +24,7 @@ options:
 If you want to do 3 sets of 10 reps with a 1 second gap between reps, you would run the following command:
 
 ```
-python3 sets_and_reps_counter.py --rep_gap 1000 --reps 10 --sets 3 --output_file 3x10.mp3
+python3 sets_and_reps_counter.py --reps 10 --sets 3 --rep_gap 1 --set_gap 30 --output_file 3x10.mp3
 ```
 
 ### Audio File Contents
@@ -33,65 +34,64 @@ Suppose you run the above command. The audio file will contain the following:
 ```
 Set 1
 1
-1 second (1000 milliseconds) gap
+1 second gap
 2
-1 second (1000 milliseconds) gap
+1 second gap
 3
-1 second (1000 milliseconds) gap
+1 second gap
 4
-1 second (1000 milliseconds) gap
+1 second gap
 5
-1 second (1000 milliseconds) gap
+1 second gap
 6
-1 second (1000 milliseconds) gap
+1 second gap
 7
-1 second (1000 milliseconds) gap
+1 second gap
 8
-1 second (1000 milliseconds) gap
+1 second gap
 9
-1 second (1000 milliseconds) gap
+1 second gap
 10
-1 second (1000 milliseconds) gap
+30 seconds gap
 Set 2
 1
-1 second (1000 milliseconds) gap
+1 second gap
 2
-1 second (1000 milliseconds) gap
+1 second gap
 3
-1 second (1000 milliseconds) gap
+1 second gap
 4
-1 second (1000 milliseconds) gap
+1 second gap
 5
-1 second (1000 milliseconds) gap
+1 second gap
 6
-1 second (1000 milliseconds) gap
+1 second gap
 7
-1 second (1000 milliseconds) gap
+1 second gap
 8
-1 second (1000 milliseconds) gap
+1 second gap
 9
-1 second (1000 milliseconds) gap
+1 second gap
 10
-1 second (1000 milliseconds) gap
+30 seconds gap
 Set 3
 1
-1 second (1000 milliseconds) gap
+1 second gap
 2
-1 second (1000 milliseconds) gap
+1 second gap
 3
-1 second (1000 milliseconds) gap
+1 second gap
 4
-1 second (1000 milliseconds) gap
+1 second gap
 5
-1 second (1000 milliseconds) gap
+1 second gap
 6
-1 second (1000 milliseconds) gap
+1 second gap
 7
-1 second (1000 milliseconds) gap
+1 second gap
 8
-1 second (1000 milliseconds) gap
+1 second gap
 9
-1 second (1000 milliseconds) gap
+1 second gap
 10
-1 second (1000 milliseconds) gap
 ```
--- sets_and_reps_counter.py
+++ sets_and_reps_counter.py
@@ -9,14 +9,18 @@ from pydub import AudioSegment
 
 def create_parser():
     parser = argparse.ArgumentParser(description='Generate a guided sets and reps audio file.')
-    parser.add_argument('--rep_gap', type=int, required=True, help='The gap between reps in milliseconds.')
     parser.add_argument('--reps', type=int, required=True, help='The number of reps in each set.')
     parser.add_argument('--sets', type=int, required=True, help='The number of sets.')
+    parser.add_argument('--rep_gap', type=int, required=True, help='The gap between reps in seconds.')
+    parser.add_argument('--set_gap', type=int, required=True, help='The gap between sets in seconds.')
     parser.add_argument('--output_file', type=str, required=False, help='The output mp3 file name.')
     return parser
 
 def get_rep_gap_silence(rep_gap: int) -> AudioSegment:
     return AudioSegment.silent(duration=rep_gap)
+
+def get_set_gap_silence(set_gap: int) -> AudioSegment:
+    return AudioSegment.silent(duration=set_gap)
 
 def pregenerate_set_announcements(sets: int) -> List[AudioSegment]:
     set_announcements = []
@@ -44,31 +48,36 @@ def create_combined_rep_announcements(rep_announcements: List[AudioSegment], rep
     combined_rep_announcements = AudioSegment.empty()
     for i, rep_announcement in enumerate(rep_announcements):
         combined_rep_announcements += rep_announcement
-        combined_rep_announcements += rep_gap_silence
+        if i < len(rep_announcements) - 1:
+            combined_rep_announcements += rep_gap_silence
     print("Created combined rep announcements")
     return combined_rep_announcements
 
-def create_combined_set_announcements(set_announcements: List[AudioSegment], combined_rep_announcements: AudioSegment) -> AudioSegment:
+def create_combined_set_announcements(set_announcements: List[AudioSegment], combined_rep_announcements: AudioSegment, set_gap_silence: AudioSegment) -> AudioSegment:
     combined_set_announcements = AudioSegment.empty()
     for set_announcement in set_announcements:
         combined_set_announcements += set_announcement
         combined_set_announcements += combined_rep_announcements
+        if set_announcement != set_announcements[-1]:
+            combined_set_announcements += set_gap_silence
     print("Created combined set announcements")
     return combined_set_announcements
 
 def create_combined_audio(args):
     sets = args.sets
     reps = args.reps
-    rep_gap = args.rep_gap
+    rep_gap = args.rep_gap * 1000
+    set_gap = args.set_gap * 1000
     if args.output_file is None:
         output_file_name = f"{reps}reps_for_{sets}sets_with_{rep_gap}ms_gap.mp3"
     else:
         output_file_name = args.output_file
     rep_gap_silence = get_rep_gap_silence(rep_gap)
+    set_gap_silence = get_set_gap_silence(set_gap)
     set_announcements = pregenerate_set_announcements(sets)
     rep_announcements = pregenerate_rep_announcements(reps)
     combined_rep_announcements = create_combined_rep_announcements(rep_announcements, rep_gap_silence)
-    combined_set_announcements = create_combined_set_announcements(set_announcements, combined_rep_announcements)
+    combined_set_announcements = create_combined_set_announcements(set_announcements, combined_rep_announcements, set_gap_silence)
     combined_set_announcements.export(output_file_name, format="mp3")
     print(f"MP3 file created: {output_file_name}")
 
