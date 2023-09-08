1890e711d3b46a6c30b3f8491f92f3ac8314d920
Sandeep Vishnu <sandeepkv93@gmail.com>
2023-09-08 07:58:10 -0700

    Make the script take outputfile name as required arg

--- sets_and_reps_counter.py
+++ sets_and_reps_counter.py
@@ -13,14 +13,8 @@ def create_parser():
     parser.add_argument('--sets', type=int, required=True, help='The number of sets.')
     parser.add_argument('--rep_gap', type=int, required=True, help='The gap between reps in seconds.')
     parser.add_argument('--set_gap', type=int, required=True, help='The gap between sets in seconds.')
-    parser.add_argument('--output_file', type=str, required=False, help='The output mp3 file name.')
+    parser.add_argument('--output_file', type=str, required=True, help='The output mp3 file name.')
     return parser
-
-def get_rep_gap_silence(rep_gap: int) -> AudioSegment:
-    return AudioSegment.silent(duration=rep_gap)
-
-def get_set_gap_silence(set_gap: int) -> AudioSegment:
-    return AudioSegment.silent(duration=set_gap)
 
 def pregenerate_set_announcements(sets: int) -> List[AudioSegment]:
     set_announcements = []
@@ -50,7 +44,6 @@ def create_combined_rep_announcements(rep_announcements: List[AudioSegment], rep
         combined_rep_announcements += rep_announcement
         if i < len(rep_announcements) - 1:
             combined_rep_announcements += rep_gap_silence
-    print("Created combined rep announcements")
     return combined_rep_announcements
 
 def create_combined_set_announcements(set_announcements: List[AudioSegment], combined_rep_announcements: AudioSegment, set_gap_silence: AudioSegment) -> AudioSegment:
@@ -60,28 +53,18 @@ def create_combined_set_announcements(set_announcements: List[AudioSegment], com
         combined_set_announcements += combined_rep_announcements
         if set_announcement != set_announcements[-1]:
             combined_set_announcements += set_gap_silence
-    print("Created combined set announcements")
     return combined_set_announcements
 
 def create_combined_audio(args):
-    sets = args.sets
-    reps = args.reps
-    rep_gap = args.rep_gap * 1000
-    set_gap = args.set_gap * 1000
-    if args.output_file is None:
-        output_file_name = f"{reps}reps_for_{sets}sets_with_{rep_gap}ms_gap.mp3"
-    else:
-        output_file_name = args.output_file
-    rep_gap_silence = get_rep_gap_silence(rep_gap)
-    set_gap_silence = get_set_gap_silence(set_gap)
-    set_announcements = pregenerate_set_announcements(sets)
-    rep_announcements = pregenerate_rep_announcements(reps)
+    rep_gap_silence = AudioSegment.silent(duration=args.rep_gap * 1000)
+    set_gap_silence = AudioSegment.silent(duration=args.set_gap * 1000)
+    set_announcements = pregenerate_set_announcements(args.sets)
+    rep_announcements = pregenerate_rep_announcements(args.reps)
     combined_rep_announcements = create_combined_rep_announcements(rep_announcements, rep_gap_silence)
     combined_set_announcements = create_combined_set_announcements(set_announcements, combined_rep_announcements, set_gap_silence)
-    combined_set_announcements.export(output_file_name, format="mp3")
-    print(f"MP3 file created: {output_file_name}")
+    combined_set_announcements.export(args.output_file, format="mp3")
+    print(f"MP3 file created: {args.output_file}")
 
 if __name__ == "__main__":
-    parser = create_parser()
-    args = parser.parse_args()
+    args = create_parser().parse_args()
     create_combined_audio(args)
